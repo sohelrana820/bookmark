72bc0bd36df469d777b274e90ebddc9f71da7180
sohelrana820 <runwithlight820>
2016-02-22 03:17:30 +0600

    Important changes

--- config/schema/structure.sql
+++ config/schema/structure.sql
@@ -1,18 +1,99 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 18, 2015 at 12:27 AM
--- Server version: 5.6.26
--- PHP Version: 5.5.28
+-- Host: localhost
+-- Generation Time: Feb 22, 2016 at 03:16 AM
+-- Server version: 5.5.46-0ubuntu0.14.04.2
+-- PHP Version: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `cakephp3-user-management`
+-- Database: `bookmark`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `boards`
+--
+
+CREATE TABLE IF NOT EXISTS `boards` (
+  `id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(32) NOT NULL,
+  `user_id` int(11) unsigned zerofill NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `description` text,
+  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 0 = Inactive, 1 = Active',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL COMMENT 'S',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `boards_resources`
+--
+
+CREATE TABLE IF NOT EXISTS `boards_resources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `board_id` int(11) NOT NULL,
+  `resource_id` int(11) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `board_id` (`board_id`),
+  KEY `resource_id` (`resource_id`),
+  KEY `resource_id_2` (`resource_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `parent_id` int(3) DEFAULT NULL,
+  `lft` int(3) NOT NULL,
+  `rght` int(3) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `slug` varchar(80) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'Status: 1 =active, 2 = Inactive',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories_resources`
+--
+
+CREATE TABLE IF NOT EXISTS `categories_resources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(3) NOT NULL,
+  `resource_id` int(11) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'Status: 1 = Active, 2 = Inactive',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`),
+  KEY `rerource_id` (`resource_id`),
+  KEY `resource_id` (`resource_id`),
+  KEY `category_id_2` (`category_id`),
+  KEY `resource_id_2` (`resource_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -21,14 +102,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `profiles` (
-  `id` int(11) unsigned zerofill NOT NULL,
+  `id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned zerofill NOT NULL,
   `created_by` int(11) unsigned zerofill DEFAULT NULL,
   `first_name` varchar(32) NOT NULL,
   `last_name` varchar(32) NOT NULL,
-  `profile_pic` varchar(255) DEFAULT NULL,
-  `gender` int(1) DEFAULT NULL COMMENT 'GENDER: 1 = Male, 2 = female',
-  `birthday` datetime DEFAULT NULL,
+  `profile_pic` varchar(128) DEFAULT NULL,
+  `gender` int(1) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
   `street_1` varchar(255) DEFAULT NULL,
   `street_2` varchar(255) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
@@ -37,10 +118,36 @@ CREATE TABLE IF NOT EXISTS `profiles` (
   `state` varchar(32) DEFAULT NULL,
   `postal_code` varchar(8) DEFAULT NULL,
   `country` varchar(32) DEFAULT NULL,
-  `bio` text,
   `created` datetime NOT NULL,
-  `modified` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `created_by` (`created_by`),
+  KEY `user_id_2` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resources`
+--
+
+CREATE TABLE IF NOT EXISTS `resources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(32) NOT NULL,
+  `user_id` int(11) unsigned zerofill NOT NULL,
+  `status` int(1) NOT NULL,
+  `label` varchar(128) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `img` varchar(128) NOT NULL,
+  `tags` text,
+  `content` text,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -49,53 +156,46 @@ CREATE TABLE IF NOT EXISTS `profiles` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) unsigned zerofill NOT NULL,
+  `id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(32) NOT NULL,
   `username` varchar(64) NOT NULL,
   `password` varchar(128) NOT NULL,
   `role` int(1) NOT NULL DEFAULT '2' COMMENT 'role: 1 = admin, 2 = general',
+  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = Active, 0 = Inactive',
+  `email_verifying_code` varchar(32) NOT NULL,
+  `forgot_pass_code` varchar(32) DEFAULT NULL,
+  `email_verify` int(1) NOT NULL DEFAULT '0' COMMENT 'email_verify: 1 = Verified, 0 =  Unverified',
   `created` datetime NOT NULL,
-  `modified` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `profiles`
---
-ALTER TABLE `profiles`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `created_by` (`created_by`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `profiles`
---
-ALTER TABLE `profiles`
-  MODIFY `id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `boards`
+--
+ALTER TABLE `boards`
+  ADD CONSTRAINT `boards_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `categories_resources`
+--
+ALTER TABLE `categories_resources`
+  ADD CONSTRAINT `categories_resources_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `categories_resources_ibfk_2` FOREIGN KEY (`resource_id`) REFERENCES `resources` (`id`);
 
 --
 -- Constraints for table `profiles`
 --
 ALTER TABLE `profiles`
   ADD CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `profiles_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD CONSTRAINT `profiles_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `resources`
+--
+ALTER TABLE `resources`
+  ADD CONSTRAINT `resources_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
